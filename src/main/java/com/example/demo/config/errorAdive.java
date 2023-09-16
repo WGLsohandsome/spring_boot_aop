07f878ba73ff9d742cf554f356436acc6cd9f252
汪高乐 <624917387@qq.com>
2023-09-16 16:17:57 +0800

    同一异常处理

--- src/main/java/com/example/demo/config/errorAdive.java
+++ src/main/java/com/example/demo/config/errorAdive.java
@@ -1,4 +1,20 @@
 package com.example.demo.config;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Objects;
+@ControllerAdvice
 public class errorAdive {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public HashMap<String, Object> exAdvice(Exception e){
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("code","-1");
+        result.put("msg",e.getMessage());
+        return result;
+    }
 }
